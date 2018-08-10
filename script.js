a4b2150d8a671534f5a66c6ee288e0d3fd25482b
Reece_Collett <rcollett@spartaglobal.com>
2018-08-10 16:23:05 +0100

    Added psuedo code for planning

---
+++ script.js
@@ -0,0 +1,16 @@
+//NO VANILLA JS, ONLY JQUERY
+
+//On click
+  //Display either a O or a X
+  //Change turn
+//Make it turn based
+//Allow for all winning conditions
+  //Lines
+  //Diagonals
+  //Draw
+//Use RND to determine who does first
+  //Display who does first
+//Allow reset of the game
+//Check for winning conditions.
+//BONUS
+  //Could create the grid and style it here.
